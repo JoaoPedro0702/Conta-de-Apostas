be15ee09cad6700d50d36af5c2c031c5368c2782
João Pedro <joaoptcantunes07@gmail.com>
2023-05-17 16:26:06 -0300

    first commit

---
+++ ApostaEsportiva.cs
@@ -0,0 +1,63 @@
+﻿using System.Globalization;
+
+namespace Course {
+    
+        class ApostaEsportivas {
+
+                public int Numero { get; private set; }
+
+                public string Nome { get; set; }
+
+                public double Saldo {get; private set; }
+
+                public string Familiar {get; private set; }
+
+                public ApostaEsportivas() {
+
+                }
+
+                public ApostaEsportivas(int numero, string nome, double saldo) {
+                        Numero = numero;
+                        Nome = nome;
+                        Saldo = saldo;
+                }
+
+                public ApostaEsportivas(int numero, string nome, double saldo, string familiar) : this(numero, nome, saldo) {
+                       Familiar = familiar;
+                }
+
+                public void Deposito(double quantia) {
+                        Saldo += quantia;
+                }
+
+                public void GanhouAposta(double ganho) {
+                        Saldo += ganho;
+                }
+
+                public void PerdeuAposta(double perda) {
+                        Saldo -= perda;
+                }
+
+                public void Saque(double quantia) {
+                        Saldo -= quantia;
+                }
+
+                public override string ToString() {
+                        return "Conta: " 
+                               + Numero 
+                               + " , Titular: " 
+                               + Nome 
+                               + " , Familiar Conjunto: " 
+                               + Familiar 
+                               + ", Saldo: " 
+                               + Saldo.ToString("F2", CultureInfo.InvariantCulture);
+                }
+
+
+
+        }
+}
+
+    
+
+    
---
+++ teste1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Globalization;
+
+namespace Course {
+    class Program {
+        static void Main(string[] args) {
+
+            ApostaEsportivas aposta = new ApostaEsportivas();
+
+            System.Console.Write("Digite seu numero de Conta: ");
+            int numero = int.Parse(Console.ReadLine());
+
+            System.Console.Write("Digite seu nome: ");
+            string nome = Console.ReadLine();
+
+            System.Console.Write("Qual quantia deseja fazer o deposito?: ");
+            double quantia = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            aposta.Deposito(quantia);
+
+            System.Console.Write("Existe outra pessoa que voce divide esta conta?(s/n): ");
+            char resp = char.Parse(Console.ReadLine());
+            if (resp == 's' || resp == 'S')  {
+                System.Console.WriteLine("Digite o nome da outra pessoa: ");
+                string familiar = Console.ReadLine();
+                aposta = new ApostaEsportivas(numero, nome, quantia, familiar);
+            }
+            else {
+                aposta = new ApostaEsportivas(numero, nome, quantia);
+            }
+
+            System.Console.WriteLine("Dados da Conta: ");
+            System.Console.WriteLine(aposta);
+
+            System.Console.Write("Ganhou dinheiro com a aposta?(s/n): ");
+            char resp2 = char.Parse(Console.ReadLine());
+            if (resp2 == 's' || resp2 == 'S')  {
+                System.Console.Write("Quanto ganhou?: ");
+                double ganho = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                aposta.GanhouAposta(ganho);
+            }    
+            else {
+                System.Console.Write("Quanto perdeu?: ");
+                double perda = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                aposta.PerdeuAposta(perda);
+            }
+
+            System.Console.WriteLine("Dados da Conta atualizados: ");
+            System.Console.WriteLine(aposta);
+
+            System.Console.WriteLine("Deseja realizar o saque?(s/n): ");
+            char resp3 = char.Parse(Console.ReadLine());
+            if (resp3 == 's' || resp3 == 'S')  {
+                System.Console.Write("Quanto deseja sacar?: ");
+                double saque = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                aposta.Saque(saque);
+                System.Console.Write(aposta);
+            }
+            else {
+                System.Console.Write(aposta);
+            }
+        }
+    }
+}
+
+            
+
+
+
